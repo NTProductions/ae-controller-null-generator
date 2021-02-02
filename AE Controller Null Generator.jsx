e5b3251c9afa71df018e273cd1af3be2583cc01c
Nate Lovell <Natelovell99@gmail.com>
2021-02-02 01:04:38 -0700

    Create AE Controller Null Generator.jsx

---
+++ AE Controller Null Generator.jsx
@@ -0,0 +1,34 @@
+﻿// ae controller null generator
+// generates based on selectedProperties
+generateControllerNull(app.project.activeItem.selectedProperties);
+
+function generateControllerNull(selectedProps) {
+    app.beginUndoGroup("Generate Controller Null");
+    
+    var props = selectedProps;
+    var comp = app.project.activeItem;
+
+    var controllerNull = getLayerNamed(comp, "Controller Null");
+    if(controllerNull == null) {
+           controllerNull = comp.layers.addNull(comp.duration);
+           controllerNull.name =  "Controller Null";
+        }
+    
+    var thisEffect;
+    for(var i = 0; i < props.length; i++) {
+        thisEffect = controllerNull.Effects.addProperty("ADBE Slider Control");
+        thisEffect.name = props[i].name;
+        props[i].expression = 'thisComp.layer("Controller Null").effect("'+thisEffect.name+'").property("Slider")';
+        }
+    
+    app.endUndoGroup();
+    }
+
+function getLayerNamed(comp, name) {
+    for(var i = 1; i <= comp.numLayers; i++) {
+        if(comp.layer(i).name == name) {
+            return comp.layer(i);
+            }
+        }
+    return null;
+    }
